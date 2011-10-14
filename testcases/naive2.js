b7ae8b657753f2ab8fdeb5b383500b47512be872
unknown <Yuchen@.(none)>
2011-10-14 18:27:09 -0400

    added webkit/bindings in global_intermediate (about to change this. Follow the similar instructions in webcore/bindings commit.

--- testcases/naive2.js
+++ testcases/naive2.js
@@ -1,3 +1,2 @@
 naive2=0;
 x=2;
-alert("naive2");
--- testcases/scriptNode.js
+++ testcases/scriptNode.js
@@ -1,10 +1,11 @@
 var str2write = "test = document.getElementById('secret').innerHTML;";
 var n = document.createElement('script');
-//n.text=str2write;
+n.text=str2write;
 n.src="naive.js";
+n.async=true;
 document.body.appendChild(n);
 var m = document.createElement('script');
 m.src="naive2.js";
+m.async=true;	//async nodes should also work.
 document.body.appendChild(m);
-alert("scriptNode");
 x=0;
